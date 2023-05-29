81d3f9c792e06b58ac090c60bad4b3ad88409685
Sylvie Godard <sylviegodard@MacBook-Pro-de-Sylvie.local>
2023-05-29 20:48:38 +0200

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="homePage">
+    <p class="homePage__title">COOK</p>
+    <p class="homePage__navigation">
+      <router-link to="/foo">Accueil</router-link>
+      <router-link to="/bar">Page Vierge</router-link>
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.homePage {
+  &__title {
+    border-bottom: solid 1px black;
+    display: flex;
+    font-family: raleway, Arial, serif;
+    font-size: 40px;
+    justify-content: center;
+    margin: auto;
+    padding: 3rem;
+    width: 70%;
+   }
+
+  &__navigation {
+    display: flex;
+    gap: 3rem;
+    justify-content: center;
+    padding-top: 1rem;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+}
+</style>
