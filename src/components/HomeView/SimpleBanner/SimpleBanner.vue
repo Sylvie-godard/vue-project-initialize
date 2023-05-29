6dd1b0ff6c649fab820278ba4b4488576b37257a
Sylvie Godard <sylviegodard@MacBook-Pro-de-Sylvie.local>
2023-05-29 21:41:25 +0200

    add component simple banner

---
+++ src/components/HomeView/SimpleBanner/SimpleBanner.vue
@@ -0,0 +1,25 @@
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'SimpleBanner'
+})
+</script>
+
+<template>
+<div class="SimpleBanner">
+  <p class="SimpleBanner__title">COOK</p>
+</div>
+</template>
+
+<style lang="scss">
+.SimpleBanner {
+  background-color: #F1F1F1;
+  padding: 8rem;
+  
+  &__title {
+    font-size: 72px;
+    text-align: center;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import HeaderHome from "@/components/Common/HeaderHome/HeaderHome.vue";
-import TitleHome from "@/components/HomeView/TitleHome/TitleHome.vue";
+import HeaderHome from '@/components/Common/HeaderHome/HeaderHome.vue';
+import TitleHome from '@/components/HomeView/TitleHome/TitleHome.vue';
+import SimpleBanner from '@/components/HomeView/SimpleBanner/SimpleBanner.vue';
+
 </script>
 
 <template>
   <div>
     <HeaderHome />
     <TitleHome />
+    <SimpleBanner />
   </div>
 </template>
