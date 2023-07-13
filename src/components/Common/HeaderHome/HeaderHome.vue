b43e5ba4b3560b60e1e890892634d1c63f607233
Sylvie <sylvie@playplay.com>
2023-07-13 14:27:05 +0200

    master: modif header

--- src/components/Common/HeaderHome/HeaderHome.vue
+++ src/components/Common/HeaderHome/HeaderHome.vue
@@ -1,13 +1,42 @@
 <script>
 import { defineComponent } from 'vue';
-
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 export default defineComponent({
-  name: 'HeaderHome'
+  name: 'HeaderHome',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  setup() {
+    const slides = [
+        'itep-eva.png', 'joe-longomo.png', 'skinny.png'
+    ]
+    return {
+      slides
+    }
+  }
 })
 </script>
 
 <template>
 <div class="HeaderHome">
+  <img src="/src/assets/itep_clan.png" alt="" class="HeaderHome__img">
+
+  <carousel :items-to-show="1.5">
+    <slide v-for="slide in 3" :key="slide" :autoplay="2000"
+           :wrap-around="true">
+      {slide}    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+
+
   <p class="HeaderHome__title">COOK</p>
   <p class="HeaderHome__navigation">
     <router-link to="/foo">Accueil</router-link>
@@ -18,6 +47,11 @@ export default defineComponent({
 
 <style lang="scss">
 .HeaderHome {
+  &__img {
+    width: 100%;
+    height: 8rem;
+    object-fit: contain;
+  }
   &__title {
     border-bottom: solid 1px black;
     display: flex;
