670207e34a19796af942c6f1f7f3d209220da259
Sylvie-godard <sylvie-godard@hotmail.fr>
2023-12-22 16:47:52 +0100

    add assets images and videos

---
+++ src/components/Common/FooterApp/FooterApp.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="FooterApp">
+        C'est Moi le footer
+    </div>
+</template>
+
+<style lang="scss">
+.FooterApp {
+    margin-top: 100px;
+    background-color: #000;
+    color: #fff;
+    padding: 20px;
+}
+</style>
